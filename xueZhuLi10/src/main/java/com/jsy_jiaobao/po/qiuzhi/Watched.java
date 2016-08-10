a8ec24ecef478b233f4b616e6a13779135efb9cb
BugHunter1234 <444811716@qq.com>
2016-08-10 09:18:40 +0800

    po\qiuzhi

--- xueZhuLi10/src/main/java/com/jsy_jiaobao/po/qiuzhi/Watched.java
+++ xueZhuLi10/src/main/java/com/jsy_jiaobao/po/qiuzhi/Watched.java
@@ -2,15 +2,13 @@ package com.jsy_jiaobao.po.qiuzhi;
 
 //被观察者  
 public interface Watched {
-	public void addWatcher(Watcher watcher);
+	void addWatcher(Watcher watcher);
 
-	public void removeWatcher(Watcher watcher);
+	void removeWatcher(Watcher watcher);
 	/***<pre>
 	 * RecommentIndexItem 推荐
 	 * QuestionIndexItem 首页和教学后
-	 * @param rEntity
-	 * @param qEntity
 	 */
-	public void notifyWatcher(QuestionIndexItem qEntity);
+	void notifyWatcher(QuestionIndexItem qEntity);
 	
 }
--- xueZhuLi10/src/main/java/com/jsy_jiaobao/po/qiuzhi/Watcher.java
+++ xueZhuLi10/src/main/java/com/jsy_jiaobao/po/qiuzhi/Watcher.java
@@ -2,6 +2,6 @@ package com.jsy_jiaobao.po.qiuzhi;
 
 //观察者  
 public interface Watcher {
-	public void update(QuestionIndexItem qEntity);
-	public void update(AnswerItem answer);
+	void update(QuestionIndexItem qEntity);
+	void update(AnswerItem answer);
 }
