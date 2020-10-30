e4f0dffad2fa4922223d5ffe69a0e1804b3fd34e
LuckyCX <721685743@qq.com>
2020-10-30 13:32:48 +0800

    no message

--- xueZhuLi10/src/main/java/com/jsy_jiaobao/main/personalcenter/PersonalInfoCollectActivity.java
+++ xueZhuLi10/src/main/java/com/jsy_jiaobao/main/personalcenter/PersonalInfoCollectActivity.java
@@ -29,6 +29,9 @@ public class PersonalInfoCollectActivity extends BaseActivity {
 
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			finish();
+		}
 		return super.onKeyDown(keyCode, event);
 	}
 }
